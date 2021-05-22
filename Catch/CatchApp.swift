4b0ef5018194f3d4d99dee95f394f53ed0acd349
Danila Goriachkin <dndv279@gmail.com>
2021-05-23 06:37:16 +1000

    Initial Commit

---
+++ Catch/CatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchApp.swift
+//  Catch
+//
+//  Created by Danila on 23.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct CatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
